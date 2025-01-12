94e288d9c132c1f43b751dbaf1813ffd6c7d129f
Viktoriia-1 <144144257+Viktoriia-1@users.noreply.github.com>
2025-01-12 15:16:25 +0100

    Добавьте файлы проекта.

---
+++ ContextBD/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.ContextBD
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions) 
+            : base(contextOptions) 
+        { 
+        
+        }
+
+        public DbSet<Employee> Employees { get; set; }    
+     
+    }
+}
